f93868eec85a54cfa850aff13d700fd2737f6438
dmarins <diogodsfmarins@gmail.com>
2024-07-23 21:28:51 -0300

    feat: main.go and middleware code

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dmarins/rate-limiter-challenge-go/config"
+	"github.com/dmarins/rate-limiter-challenge-go/limiter"
+	"github.com/dmarins/rate-limiter-challenge-go/middleware"
+	"github.com/go-redis/redis/v8"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: cfg.RedisAddr,
+	})
+
+	rl := limiter.NewRateLimiter(
+		rdb,
+		cfg.RateLimitIP,
+		cfg.RateLimitToken,
+		time.Duration(cfg.BlockTimeIP)*time.Second,
+		time.Duration(cfg.BlockTimeToken)*time.Second,
+	)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	handler := middleware.RateLimiterMiddleware(rl)(mux)
+
+	http.ListenAndServe(":8080", handler)
+}
---
+++ middleware/middleware.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/dmarins/rate-limiter-challenge-go/limiter"
+)
+
+func RateLimiterMiddleware(rl *limiter.RateLimiter) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ip := strings.Split(r.RemoteAddr, ":")[0]
+			token := r.Header.Get("API_KEY")
+
+			allowed, err := rl.Allow(ip, token)
+			if err != nil {
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+
+			if !allowed {
+				http.Error(w, "you have reached the maximum number of requests or actions allowed within a certain time frame", http.StatusTooManyRequests)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
